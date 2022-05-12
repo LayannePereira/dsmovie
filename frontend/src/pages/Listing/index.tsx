29edcb0bb296e1f64006317d900ff040f31e1d72
Layanne Pereira <Layannepereiraa28@gmail.com>
2022-05-12 17:02:19 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,26 +5,26 @@ import MovieCard from "components/MovieCard";
 function Listing() {
     return (
         <>
-          <Pagination/>
+          <Pagination />
           <div className="container">
           <div className="row">
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
             <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard/>
+              <MovieCard />
             </div>
           
 
